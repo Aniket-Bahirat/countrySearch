265a4c36c64cf524f784e72c5593b0013bfb991f
Aniket-Bahirat <aniketbahirat2525@gmail.com>
2024-08-31 17:22:09 +0530

    ss

--- OneDrive/Desktop/whether/Whether/src/App.js
+++ OneDrive/Desktop/whether/Whether/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
+      <Weather/>
       
     </div>
   );
